5ccd0109d0fcbe48751ce3c8d3a26fdbc04bd33b
Patrick Arminio <patrick.arminio@gmail.com>
2014-12-27 14:27:19 +0100

    Added simple route parser utility method.

--- app/src/androidTest/java/it/patrick91/orariair/test/TestJSONParsing.java
+++ app/src/androidTest/java/it/patrick91/orariair/test/TestJSONParsing.java
@@ -6,9 +6,11 @@ import android.test.AndroidTestCase;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import it.patrick91.orariair.data.AirContract;
 import it.patrick91.orariair.sync.ParsingUtils;
 
 import static it.patrick91.orariair.data.AirContract.LocalityEntry;
+import static it.patrick91.orariair.data.AirContract.RouteEntry;
 
 /**
  * Created by patrick on 24/12/14.
@@ -23,5 +25,28 @@ public class TestJSONParsing extends AndroidTestCase {
 
         assertEquals(values.getAsString(LocalityEntry.COLUMN_NAME), "Avellino");
         assertEquals(values.getAsInteger(LocalityEntry.COLUMN_API_ID).intValue(), 12);
+    }
+
+    public void testSingleRouteParsing() throws JSONException {
+        String routeJSONString = "{" +
+            "\"stops\": [{" +
+                "\"time\": \"08:00\"," +
+                "\"name\": \"AVELLINO - P.zza Kennedy\"" +
+            "}, {" +
+                "\"time\": \"08:35\"," +
+                "\"name\": \"FISCIANO (SA)\"" +
+            "}]," +
+            "\"start_time\": \"08:00\"," +
+            "\"end_time\": \"08:35\"," +
+            "\"duration\":\"00:35\"" +
+        "}";
+
+        JSONObject obj = new JSONObject(routeJSONString);
+
+        ContentValues values = ParsingUtils.parseRoute(obj);
+
+        assertEquals(values.getAsString(RouteEntry.COLUMN_START_TIME), "08:00");
+        assertEquals(values.getAsString(RouteEntry.COLUMN_END_TIME), "08:35");
+        assertEquals(values.getAsString(RouteEntry.COLUMN_DURATION), "00:35");
     }
 }
--- app/src/main/java/it/patrick91/orariair/sync/ParsingUtils.java
+++ app/src/main/java/it/patrick91/orariair/sync/ParsingUtils.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 import it.patrick91.orariair.data.AirContract;
 
 import static it.patrick91.orariair.data.AirContract.LocalityEntry;
+import static it.patrick91.orariair.data.AirContract.RouteEntry;
 
 /**
  * Created by patrick on 24/12/14.
@@ -18,6 +19,16 @@ public class ParsingUtils {
 
         values.put(LocalityEntry.COLUMN_NAME, obj.getString("name"));
         values.put(LocalityEntry.COLUMN_API_ID, obj.getInt("pk"));
+
+        return values;
+    }
+
+    public static ContentValues parseRoute(JSONObject obj) throws JSONException {
+        ContentValues values = new ContentValues();
+
+        values.put(RouteEntry.COLUMN_START_TIME, obj.getString("start_time"));
+        values.put(RouteEntry.COLUMN_END_TIME, obj.getString("end_time"));
+        values.put(RouteEntry.COLUMN_DURATION, obj.getString("duration"));
 
         return values;
     }
